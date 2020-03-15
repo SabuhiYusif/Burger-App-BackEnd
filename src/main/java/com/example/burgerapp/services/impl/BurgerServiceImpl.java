638f7f505aef0a7097136309e24f3f662421690c
sbh <sebuhi11@gmail.com>
2020-03-15 18:39:56 +0400

    Bosluq

--- src/main/java/com/example/burgerapp/services/impl/BurgerServiceImpl.java
+++ src/main/java/com/example/burgerapp/services/impl/BurgerServiceImpl.java
@@ -52,6 +52,7 @@ public class BurgerServiceImpl implements BurgerService {
 
         return photoUrls;
     }
+    
 
     @Override
     public List<BurgerVenueInfo> getBurgerVenuesInfo(VenueResult venueResult, List<BurgerVenueInfo> burgerVenues) {
